334bc08a4765fc2b17c8deffffe60cbb75673509
dr1zzt2 <dr1zzt2.NK@gmail.com>
2018-05-09 09:20:46 +0200

    joke function

--- gittie.py
+++ gittie.py
@@ -1,2 +1,6 @@
 def introduce
     return print("Hello, I'm Gittie!")
+def joke
+    print("Tudod mi a különbség a vidra, a kobra és a hidra között?")
+    print("Hát... a vidra és a kobra rá tud menni a hidra,")
+    print("de a kobra és a hidra, nem tud rámenni a vidra.")
